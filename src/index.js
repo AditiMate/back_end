6037c548903a611142780e26c86c48efab88ca50
AditiMate <aditimate0075@gmail.com>
2024-03-11 21:32:53 +0530

    connect to mongoDb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import dotenv from "dotenv"
+//import dotenv from "dotenv"
 import connectDB  from "./db/index.js"
 
-dotenv.config({
-    path: './env'
-})
+/*Since Node v20.6.0, node is supporting env file without any 3rd party library (dotenv). You have to just add --env-file in your script tag in package.json. So, your script tag will look like:
+
+  "scripts": {
+    "start": "node --env-file=.env src/index.js",
+    "dev": "nodemon --env-file=.env src/index.js"
+  }
+*/
+
+// dotenv.config({
+//     path: './env'
+// })
 
 
 connectDB()
